9044593a5c609f05509d25e3079929826dc1c0ce
Chana <chana987@gmail.com>
2019-11-04 12:10:33 +0200

    added routes

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.SERVER_PORT || 3000
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
 
-mongoose.connect("mongodb://localhost/citiesDB", {
+mongoose.connect("mongodb://localhost/weatherDB", {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 	useFindAndModify: false
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,12 +1,46 @@
 const express = require('express')
 const router = express.Router()
+const requestPromise = require('request-promise')
+const config = require('../config.js')
+const weatherApiKey = config.WEATHER_KEY
 
 const City = require('../models/City')
 
-router.get('/city', function (req, res) {
-    City.find({}, function (err, city) {
-        res.send(city)
+router.get('/city/:cityName', async function (req, res) {
+    let data = await requestPromise(`http://api.openweathermap.org/data/2.5/find?q=${req.params.cityName}&units=metric&APPID=${weatherApiKey}`)
+    try {
+        res.send(JSON.parse(data).list)
+    } catch {
+        throw new Error("We can't seem to find this city, please check spelling")
+    }
+})
+
+router.get('/cities', function (req, res) {
+    City.find({})
+    .exec(data => res.send(data))
+})
+
+router.post('/city', async function(req, res) {
+    let city = new City({
+        name: req.body.name,
+        temperature: req.body.main.temp,
+        condition: req.body.weather.main,
+        conditionPic: req.body.weather.icon
     })
+    city.save()
+    .then( res.send(`${city.name} added to database`) )
+})
+
+router.delete('/city/:cityName', function(req, res) {
+    try {
+        City.findOneAndDelete({
+            name: req.params.cityName
+        })
+        .then(res.send(`${req.params.cityName} removed from database`))
+    } catch {
+        res.send("This city is not saved")
+    }
+    
 })
 
 module.exports = router
