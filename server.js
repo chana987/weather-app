840523c335b859441a56f0d67ca016726b4231f5
Chana <chana987@gmail.com>
2019-11-05 10:47:34 +0200

    heroku stuff

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const path = require("path")
 const app = express()
 const api = require("./server/routes/api")
-const port = process.env.SERVER_PORT || 3000
+const port = 3000
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
 
@@ -19,4 +19,4 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use("/", api)
 
-app.listen(port, () => console.log(`Running server on port ${port}`))
+app.listen(process.env.SERVER_PORT || port, () => console.log(`Running server on port ${port}`))
